b2c4f18e203272a8b448bc6736eeac617bce76f8
live-by-unix <262261208+live-by-unix@users.noreply.github.com>
2026-08-04 19:03:03 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,32 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { base44 } from '@base44/vite-plugin';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
 
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     base44({
       // Support for legacy code that imports the base44 SDK with @/integrations, @/entities, etc.
-      // can be removed if the code has been updated to use the new SDK imports from @base44/sdk
-      legacySDKImports: process.env.BASE44_LEGACY_SDK_IMPORTS === 'true',
+      // Can be removed once your project has been migrated to @base44/sdk imports.
+      legacySDKImports:
+        process.env.BASE44_LEGACY_SDK_IMPORTS === 'true',
+
       hmrNotifier: true,
       navigationNotifier: true,
       analyticsTracker: true,
-      visualEditAgent: true
+      visualEditAgent: true,
     }),
+
     react(),
-  ]
+  ],
+
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
 });
